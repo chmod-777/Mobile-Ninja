ee38408c05b0b32e69f8a7fe1510ebdb607a7f2a
chmod-777 <sales@webgems.info>
2015-11-07 18:32:41 +0200

    added working list view with click listener

--- app/src/main/java/com/webgems/mint/mobileninja/MainActivity.java
+++ app/src/main/java/com/webgems/mint/mobileninja/MainActivity.java
@@ -4,6 +4,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +18,34 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] ussdCodes = {"Check Airtime", "Buy Whatsapp Bundle", "Buy Facebook Bundle", "Buy Data Bundle", "Buy SMS Bundle", "Send Call me back", "Ecocash"};
+
+        ListAdapter myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, ussdCodes);
+        ListView ussdListView = (ListView) findViewById(R.id.ussdListView);
+        ussdListView.setAdapter(myAdapter);
+
+        // OnClick Listener for launching call intent
+        ussdListView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        String ussdCode = String.valueOf(adapterView.getItemAtPosition(i));
+
+                    }
+                }
+        );
+
+        // OnClick Listener echoing out the text of item clicked using a Toast with make text
+//        ussdListView.setOnItemClickListener(
+//                new AdapterView.OnItemClickListener() {
+//                    @Override
+//                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                        String ussdCode = String.valueOf(adapterView.getItemAtPosition(i));
+//                        Toast.makeText(MainActivity.this, ussdCode, Toast.LENGTH_LONG).show();
+//                    }
+//                }
+//        );
     }
 
 
